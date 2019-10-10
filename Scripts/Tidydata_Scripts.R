1652c770380358af1870c1da6d387cde4591f6ff
Giles <car618@csiro.au>
2019-10-10 15:12:56 +1030

    Import data, remove unwanted columns

--- Scripts/Tidydata_Scripts.R
+++ Scripts/Tidydata_Scripts.R
@@ -1,2 +1,15 @@
 library(tidyverse)
 
+OTU_Table <- read_csv("Data/OTUtable_16S.csv", skip = 1:2)
+
+OTU_Table
+
+view(OTU_Table)
+
+OTU_Table_Bacteria <- select(OTU_Table, -2, -3, -4, -6, -8, -10, -12, -14, -15, -17, -18)
+
+OTU_Table_Bacteria
+
+view(OTU_Table_Bacteria)
+
+
